b2ccbf442d142eb687f1af5183bdf60b3fb2f939
berzanozgenc <106954118+berzanozgenc@users.noreply.github.com>
2022-06-08 17:39:13 +0300

    Add project files.

---
+++ ArdLibrary/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArdLibrary.Entities
+{
+    public class Book
+    {
+        [Key] // otomatik olarak identity yapmak için
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public int PublishYear { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public bool IsBorrowed { get; set; }
+
+    }
+}
---
+++ ArdLibrary/Entities/Borrow.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArdLibrary.Entities
+{
+    public class Borrow
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime ExpDate { get; set; }
+        public int AuthorName { get; set; } // bir kere foreignkey verirsen digerlerine erişebilirsin.
+
+        public int Title { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("BookId")]
+        public virtual Book Book { get; set; }
+
+    }
+}
